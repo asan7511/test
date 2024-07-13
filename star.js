e0d754c241b8ffa347466e18716085cac3a4157c
asan7511 <141273953+asan7511@users.noreply.github.com>
2024-07-13 23:03:10 +0200

    Add files via upload

--- star.js
+++ star.js
@@ -1,9 +1,29 @@
 var punkteElement = document.querySelector("#punkte");
-var sterne = 0;
-punkteElement.innerHTML = sterne;
+var resetElement = document.querySelector("#reset");
 var starElement = document.querySelector("#starimg");
-function starclick(){
-    console.log(sterne)
-    sterne = sterne + 1
+var sterne = 0;
+if (!(localStorage.punkte == null)){
+    sterne = parseInt(localStorage.getItem("punkte"));
 }
+
+punkteElement.innerHTML = sterne;
+
+function starclick(){
+    sterne = sterne + 1
+    punkteElement.innerHTML = sterne;
+    localStorage.setItem("punkte", sterne.toString());
+     
+    
+
+}
+
+function resetclick(){
+    console.log("hallo welt")
+    //sterne = 0
+    //punkteElement.innerHTML = sterne;
+    //localStorage.setItem("punkte", sterne.toString());
+
+}
+
+resetElement.addEventListener("click", resetclick)
 starElement.addEventListener("click", starclick)
